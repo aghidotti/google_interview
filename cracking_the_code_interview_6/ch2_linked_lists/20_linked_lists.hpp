112e9ea7011dbf8799d0cb7a6d76666522259503
Alessandro Ghidotti Piovan <aghidotti@gmail.com>
2018-08-17 17:53:57 +0200

    Add custom implementation of linked lists to match examples on the book

---
+++ cracking_the_code_interview_6/ch2_linked_lists/20_linked_lists.hpp
@@ -0,0 +1,63 @@
+#ifndef LINKED_LISTS_H
+#define LINKED_LISTS_H
+
+#include <memory>
+#include <iostream>
+
+template <typename T>
+class slist_node {
+        slist_node() = default;
+
+public:
+        const T val;
+        std::shared_ptr<slist_node<T>> next = nullptr;
+        slist_node(const T &v) : val(v) {}
+};
+
+
+template <typename T>
+class dlist_node : public slist_node<T> {
+        dlist_node() = default;
+public:
+        std::shared_ptr<dlist_node<T>> prev = nullptr;
+        dlist_node(const T &v) : slist_node<T>(v) {}
+};
+
+
+template <typename T>
+void
+push_front(std::shared_ptr<slist_node<T>> & head, const T & val) {
+        auto new_node = std::make_shared<slist_node<T>>(val);
+        if(head == nullptr)
+                head = new_node;
+        else {
+                new_node->next = head;
+                head = new_node;
+        }
+}
+
+template <typename T>
+void
+push_front(std::shared_ptr<dlist_node<T>> & head, const T & val) {
+        auto new_node = std::make_shared<dlist_node<T>>(val);
+        if(head == nullptr)
+                head = new_node;
+        else {
+                new_node->next = head;
+		head->prev = new_node;
+                head = new_node;
+        }
+}
+
+template <typename T>
+void
+print_list(std::shared_ptr<slist_node<T>> & list) {
+        auto node = list;
+        while(node != nullptr) {
+                std::cout << node->val << std::endl;
+                node = node->next;
+        }
+}
+
+
+#endif
